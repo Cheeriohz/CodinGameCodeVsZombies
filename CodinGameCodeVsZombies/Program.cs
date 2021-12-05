9e49493f03e97552344631233c6db011419ef15e
Douglas Bonomo <dwbono01@gmail.com>
2021-12-05 11:32:30 -0500

    copied zome utility code from mad pod

--- CodinGameCodeVsZombies/Program.cs
+++ CodinGameCodeVsZombies/Program.cs
@@ -47,3 +47,57 @@ class Player
         }
     }
 }
+
+public static class Utility
+{
+    public static Double CalculateDistance(int x, int y, int x2, int y2) => Math.Sqrt(Math.Pow(x2 - x, 2) + Math.Pow(y2 - y, 2));
+    public static int CalculateDistanceAsInt(int x, int y, int x2, int y2) => (int)Math.Floor(Utility.CalculateDistance(x, y, x2, y2));
+    public static int CalculateMinimum180AngleBetweenTwo360Angles(int firstAngle, int secondAngle) => (secondAngle - firstAngle).NormalizeAngleTo180();
+    public static bool IsCollisionImminent(int jx, int jy, int jvx, int jvy, int kx, int ky, int kvx, int kvy, int collisionDistance = 1000)
+    {
+        int jXPost = jx + jvx;
+        int jYPost = jy + jvy;
+        int kXPost = kx + kvx;
+        int kYPost = ky + kvy;
+
+        int distance = CalculateDistanceAsInt(jXPost, jYPost, kXPost, kYPost);
+
+        //Console.Error.WriteLine($"({jx},{jy}) + ({jvx},{jvy}) => ({jXPost},{jYPost})");
+        //Console.Error.WriteLine($"({kx},{ky}) + ({kvx},{kvy}) => ({kXPost},{kYPost})");
+        //Console.Error.WriteLine($"Distance {distance} collision {(distance <= 850 ? "Imminent" : "Not Imminent")}");
+
+        return distance <= collisionDistance;
+    }
+    public static int NormalizeAngleTo360(this int angle) => (angle + 360) % 360;
+    public static int NormalizeAngleTo180(this int angle)
+    {
+        int threeSixtyAngle = NormalizeAngleTo360(angle);
+        return threeSixtyAngle > 180 ? Math.Abs(threeSixtyAngle - 360) : threeSixtyAngle;
+    }
+    public static (int x, int y) GetShortVectorForRotateFromDegreesAngle(int x, int y, int angle)
+    {
+        double xAbsorb = Math.Cos(angle.ToRadian());
+        double yAbsorb = Math.Sin(angle.ToRadian());
+
+        return (x + (int)Math.Floor(100 * xAbsorb), y + (int)Math.Floor(100 * yAbsorb));
+
+    }
+    public static int GetSignedComplementOfAngle(int angle) => angle < 0 ? (-180 - angle) : (180 - angle);
+    public static int ToDegrees(this double val) => (int)Math.Floor(val * (180 / Math.PI));
+    public static double ToRadian(this int val) => (double)val / 180 * Math.PI;
+    public static int DetermineRotationalAngleToPoint(int x, int y, int targetX, int targetY, int currentFacingAngle) => Math.Abs(currentFacingAngle - DeterminePositionAngleToPoint(x, y, targetX, targetY).NormalizeAngleTo360()).NormalizeAngleTo180();
+    public static int DeterminePositionAngleToPoint(int x, int y, int targetX, int targetY) => Math.Atan2(targetY - y, targetX - x).ToDegrees();
+    public static int BallparkTraversalValue(int x, int y, int tx, int ty)
+    {
+        return CalculateDistanceAsInt(x, y, tx, ty);
+    }
+    public static (int x, int y) ProjectDistanceFromPointToPoint(int x, int y, int tX, int tY, double skewLength)
+    {
+        double angleToCheckPoint = Math.Atan2(tY - y, tX - x);
+
+        double projectedX = x + (skewLength * Math.Cos(angleToCheckPoint));
+        double projectedY = y + (skewLength * Math.Sin(angleToCheckPoint));
+
+        return ((int)Math.Floor(projectedX), (int)Math.Floor(projectedY));
+    }
+}
